be0534305e35c17dd587c400ac1591bc0e3c8e1a
Arturkaktus <11235813ArKa>
2025-08-04 22:52:51 +0500

    визуал кнопок управления

--- src/components/Splitter/Splitter.tsx
+++ src/components/Splitter/Splitter.tsx
@@ -2,15 +2,56 @@ import "./Splitter.css";
 import { Splitter, SplitterPanel } from "primereact/splitter";
 import TreeViewComponent from "../TreeView/TreeView";
 import TableViewComponent from "../TableView/TableView";
+import { Button } from "primereact/button";
 
 export default function SplitterComponent() {
   return (
     <Splitter className="main-splitter">
-      <SplitterPanel className="flex overflow-hidden">
-        <TreeViewComponent />
+      <SplitterPanel className="flex flex-column overflow-hidden">
+        <div className="flex gap-2 p-2">
+          <Button 
+            icon="pi pi-plus" 
+            style={{ borderRadius: '6px' }}
+            tooltip="Новый" 
+            tooltipOptions={{ position: 'bottom' }}
+          />
+          <Button 
+            icon="pi pi-pencil" 
+            style={{ borderRadius: '6px' }}
+            tooltip="Редактировать" 
+            tooltipOptions={{ position: 'bottom' }}
+          />
+          <Button 
+            icon="pi pi-trash" 
+            style={{ borderRadius: '6px' }}
+            tooltip="Удалить" 
+            tooltipOptions={{ position: 'bottom' }}
+          />
+        </div>
+        <TreeViewComponent/>
       </SplitterPanel>
-      <SplitterPanel className="flex overflow-hidden">
-        <TableViewComponent />
+      <SplitterPanel className="flex flex-column overflow-hidden">
+        <div className="flex gap-2 p-2">
+          <Button 
+            icon="pi pi-plus" 
+            style={{ borderRadius: '6px' }}
+            tooltip="Новый" 
+            tooltipOptions={{ position: 'bottom' }}
+          />
+          <Button 
+            icon="pi pi-pencil" 
+            style={{ borderRadius: '6px' }}
+            tooltip="Редактировать" 
+            tooltipOptions={{ position: 'bottom' }}
+          />
+          <Button 
+            icon="pi pi-trash" 
+            style={{ borderRadius: '6px' }}
+            tooltip="Удалить" 
+            tooltipOptions={{ position: 'bottom' }}
+          />
+        </div>
+        <TableViewComponent/>
       </SplitterPanel>
     </Splitter>
   );
